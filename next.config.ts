afe65463ac60e570822c1033a7a8905a8689a5ca
PilarMora <a22310352@ceti.mx>
2025-11-02 21:34:17 -0600

    Interfaz con imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'campestre.media',          
+      'upload.wikimedia.org',    
+      'img.freepik.com'
+    ], 
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
