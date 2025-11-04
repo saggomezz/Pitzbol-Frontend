2806332235eddc2b630b782db4b86cdbb3230168
PilarMora <a22310352@ceti.mx>
2025-11-03 23:16:39 -0600

    Imagenes de GDL

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,47 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'images.unsplash.com',
-      'campestre.media',          
-      'upload.wikimedia.org',    
-      'img.freepik.com'
-    ], 
+    // Esta es la nueva sintaxis recomendada que reemplaza a 'domains'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'museocabanas.jalisco.gob.mx', // <-- Añadido
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'campestre.media',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-tc.galaxy.tf', // <-- Añadido (de "Centro de Guadalajara")
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com', // <-- Añadido (de "Nieves de Garrafa")
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com', // <-- Mantenido de tu config anterior
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.liderempresarial.com', // <-- Aquí está el nuevo dominio
+      },
+      {
+        protocol: 'https',
+        hostname: 'visitmexico.com', // <-- ¡Añadido!
+      },
+    ],
   },
 };
 
 module.exports = nextConfig;
+
