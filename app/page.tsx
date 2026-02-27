c1a31d571080f6007da8b55cd38e4c6fe549b295
PilarMora <pilarmorag2004@hotmail.com>
2026-02-27 17:48:10 -0600

    Remove target blank from IA button

--- app/page.tsx
+++ app/page.tsx
@@ -414,8 +414,6 @@ function HomeContent() {
 
                 <a
                   href="http://69.30.204.56:3002"
-                  target="_blank"
-                  rel="noopener noreferrer"
                   className="w-full text-center py-4 rounded-2xl font-black text-xs uppercase tracking-widest transition-all flex items-center justify-center gap-3 shadow-xl bg-gradient-to-r from-[#F00808] via-[#D10606] to-[#B90000] text-white hover:shadow-2xl hover:scale-[1.02] active:scale-95 hover:from-[#D10606] hover:via-[#B90000] hover:to-[#F00808]"
                   style={{ fontFamily: "'Jockey One', sans-serif" }}
                 >
