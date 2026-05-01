aa9dc5b3e771ff34cf0ac005f9eb5cd57aa10a5e
PilarMora <pilarmorag2004@hotmail.com>
2026-04-30 20:51:19 -0600

    Eliminar línea duplicada de shuffleArr

--- app/page.tsx
+++ app/page.tsx
@@ -663,7 +663,6 @@ function HomeContent() {
         return { name: row['Nombre del Lugar'] as string, img: csvImg || getPlaceImageByCategory(firstCat), categoria: firstCat };
       };
       const shuffleArr = (arr: any[]): any[] => {
-      const shuffleArr = <T>(arr: T[]): T[] => {
         const a = [...arr];
         for (let i = a.length - 1; i > 0; i--) { const j = Math.floor(Math.random() * (i + 1)); [a[i], a[j]] = [a[j], a[i]]; }
         return a;
