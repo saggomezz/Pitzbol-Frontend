a31fe4c6eede38cd598151c18c7035c260e9ce81
brendap21 <bpna16100224@gmail.com>
2026-05-15 19:50:09 -0600

    feat: update Content Security Policy to include Cloudflare Insights sources

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,8 @@ const connectSrc = [
   'https://tile.openstreetmap.org',
   'https://*.tile.openstreetmap.org',
   'https://nominatim.openstreetmap.org',
+  'https://cloudflareinsights.com',
+  'https://static.cloudflareinsights.com',
   ...(process.env.NODE_ENV === 'development'
     ? [
         'http://localhost:3001',
@@ -55,7 +57,7 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: blob: https:",
               "font-src 'self' https://fonts.gstatic.com",
